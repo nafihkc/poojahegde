98246a3ca0d4a3c02ca35010cca7f2ec77ef415c
TEZZA <93602145+Tezzarobot@users.noreply.github.com>
2022-08-18 20:38:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('📢 ᴜᴘᴅᴀᴛᴇs', url='https://t.me/gw_links')
+                InlineKeyboardButton('📢 ᴜᴘᴅᴀᴛᴇs', url='https://t.me/gwbotschannel')
             ],
             [
                 InlineKeyboardButton('📚 ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
